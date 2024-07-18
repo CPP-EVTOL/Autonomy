c79523b1d394957a44b08ae78bd0f8a6d711a5e8
expodecay <rick.ramirez6174@gmail.com>
2024-07-18 13:31:28 -0700

    update

--- autonomy_setup.sh
+++ autonomy_setup.sh
@@ -2,7 +2,7 @@
 apt update -y
 apt upgrade -y
 git clone https://github.com/PX4/PX4-Autopilot.git --recursive
-bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
+bash ./PX4-Autopilot/Tools/setup/ubuntu.sh -y
 cd PX4-Autopilot/
 make px4_sitl
 
